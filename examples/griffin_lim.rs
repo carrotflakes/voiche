c774f1eff49bfe6f478ec0a564db27f47e64ac28
carrotflakes <carrotflakes@gmail.com>
2023-01-18 03:54:26 +0900

    fix griffin_lim

--- examples/griffin_lim.rs
+++ examples/griffin_lim.rs
@@ -1,7 +1,10 @@
 mod wav;
 
 use rustfft::num_complex::Complex;
-use voiche::{fft::Fft, griffin_lim::griffin_lim, transform::transform, windows};
+use voiche::{
+    fft::{fix_scale, Fft},
+    windows,
+};
 
 fn main() {
     let file = std::env::args()
@@ -14,24 +17,31 @@ fn main() {
 
     let start = std::time::Instant::now();
     let window_size = 1024;
-    let window = windows::hann_window(window_size);
+    let window: Vec<f32> = windows::hann_window(window_size);
     let slide_size = window_size / 4;
 
     let fft = Fft::new(window_size);
+    let transformer = Transformer::new(window, slide_size, fft);
     let bufs: Vec<_> = bufs
         .iter()
         .map(|buf| {
-            transform(
-                slide_size,
-                window.clone(),
-                |buf| {
-                    fft.retouch_spectrum(buf, |buf| {
-                        let b: Vec<_> = buf.iter().map(|&x| Complex::new(x.re, 0.0)).collect();
-                        buf.copy_from_slice(&griffin_lim(&fft, 20, &b));
-                    })
-                },
-                buf,
-            )
+            let norms = transformer.forward_norm(buf);
+
+            let mut angles: Vec<Vec<_>> = norms
+                .iter()
+                .map(|x| {
+                    x.iter()
+                        .map(|x| (x * 543.0 + 12.0) % std::f32::consts::TAU)
+                        .collect()
+                })
+                .collect();
+
+            for _ in 0..32 {
+                let buf = transformer.inverse(buf.len(), &norms, &angles);
+                angles = transformer.forward_angle(&buf);
+            }
+
+            transformer.inverse(buf.len(), &norms, &angles)
         })
         .collect();
 
@@ -39,4 +49,61 @@ fn main() {
     dbg!(wav::power(&bufs[0]));
 
     wav::save(file.replace(".", "_gl."), spec, bufs);
+}
+
+pub struct Transformer {
+    window: Vec<f32>,
+    slide_size: usize,
+    fft: Fft<f32>,
+}
+
+impl Transformer {
+    pub fn new(window: Vec<f32>, slide_size: usize, fft: Fft<f32>) -> Self {
+        Self {
+            window,
+            slide_size,
+            fft,
+        }
+    }
+
+    pub fn forward_norm(&self, buf: &[f32]) -> Vec<Vec<f32>> {
+        self.forward(buf, |c| c.norm())
+    }
+
+    pub fn forward_angle(&self, buf: &[f32]) -> Vec<Vec<f32>> {
+        self.forward(buf, |c| c.arg())
+    }
+
+    fn forward(&self, buf: &[f32], f: impl Fn(Complex<f32>) -> f32) -> Vec<Vec<f32>> {
+        buf.windows(self.window.len())
+            .step_by(self.slide_size)
+            .map(|b| {
+                let mut spec = b
+                    .iter()
+                    .zip(self.window.iter())
+                    .map(|(x, y)| Complex::new(x * y, 0.0))
+                    .collect();
+                self.fft.forward(&mut spec);
+                spec.into_iter().map(&f).collect()
+            })
+            .collect()
+    }
+
+    pub fn inverse(&self, size: usize, norms: &Vec<Vec<f32>>, angles: &Vec<Vec<f32>>) -> Vec<f32> {
+        let output_scale = self.slide_size as f32 / self.window.iter().copied().sum::<f32>();
+        let mut buf = vec![0.0; size];
+        for (i, (norm, angle)) in norms.iter().zip(angles.iter()).enumerate() {
+            let mut spec = norm
+                .iter()
+                .zip(angle.iter())
+                .map(|(&n, &a)| Complex::from_polar(n, a))
+                .collect();
+            self.fft.inverse(&mut spec);
+            fix_scale(&mut spec);
+            for (j, (x, y)) in spec.iter().zip(self.window.iter()).enumerate() {
+                buf[i * self.slide_size + j] += output_scale * x.re * y;
+            }
+        }
+        buf
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod fft;
 pub mod float;
-pub mod griffin_lim;
 pub mod pitch_shift;
 pub mod transform;
 pub mod voice_change;
