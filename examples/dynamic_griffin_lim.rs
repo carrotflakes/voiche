e5b031712b22de96938b40fcb178088dc32b10dd
carrotflakes <carrotflakes@gmail.com>
2023-01-20 03:57:46 +0900

    dynamic_griffin_lim estimates phase

--- examples/dynamic_griffin_lim.rs
+++ examples/dynamic_griffin_lim.rs
@@ -47,8 +47,9 @@ fn dynamic_griffin_lim(
     slide_size: usize,
     iterate: usize,
 ) -> impl FnMut(&mut [f32]) {
-    let fft = Fft::new(window.len());
-    let mut specs = vec![vec![[0.0, 0.0]; window.len()]];
+    let len = window.len();
+    let fft = Fft::new(len);
+    let mut specs = vec![vec![[0.0, 0.0]; len]];
 
     move |buf: &mut [f32]| {
         // Get spectrum from input
@@ -58,7 +59,23 @@ fn dynamic_griffin_lim(
             .map(|(x, y)| Complex::new(x * y, 0.0))
             .collect();
         fft.forward(&mut spec);
-        specs.push(spec.iter().map(|x| [x.norm(), 0.0]).collect());
+        let prev = &specs[0];
+        let phase_factor = std::f32::consts::TAU * slide_size as f32 / len as f32;
+        specs.push(
+            spec.iter()
+                .enumerate()
+                .map(|(i, x)| {
+                    // Estimate phase from prev
+                    let phase_diff = if i < len / 2 {
+                        i as f32
+                    } else {
+                        i as f32 - len as f32
+                    } * phase_factor;
+                    let phase = prev[i][1] + phase_diff;
+                    [x.norm(), phase]
+                })
+                .collect(),
+        );
 
         for _ in 0..iterate {
             // Reconstruct waveform from spectrogram
@@ -66,7 +83,7 @@ fn dynamic_griffin_lim(
 
             // Update phases
             waveform
-                .windows(window.len())
+                .windows(len)
                 .step_by(slide_size)
                 .zip(specs.iter_mut())
                 .for_each(|(b, p)| {
@@ -98,12 +115,15 @@ fn reconstruct(
     window: &[f32],
     slide_size: usize,
     fft: &Fft<f32>,
-    spec: &[Vec<[f32; 2]>],
+    specs: &[Vec<[f32; 2]>],
 ) -> Vec<f32> {
     let output_scale = slide_size as f32 / window.iter().copied().sum::<f32>();
-    let mut output = vec![0.0; window.len() + slide_size * (spec.len() - 1)];
-    for (i, p) in spec.iter().enumerate() {
-        let mut spec = p.iter().map(|&[n, a]| Complex::from_polar(n, a)).collect();
+    let mut output = vec![0.0; window.len() + slide_size * (specs.len() - 1)];
+    for (i, spec) in specs.iter().enumerate() {
+        let mut spec = spec
+            .iter()
+            .map(|&[n, a]| Complex::from_polar(n, a))
+            .collect();
         fft.inverse(&mut spec);
         fix_scale(&mut spec);
         for (j, (x, y)) in spec.iter().zip(window.iter()).enumerate() {
