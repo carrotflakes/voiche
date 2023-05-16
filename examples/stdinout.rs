26f72b5cb33fe91995853cd4cf57f19bfa7df8a5
carrotflakes <carrotflakes@gmail.com>
2023-05-17 03:43:19 +0900

    fix parameters in stdinout.rs

--- examples/stdinout.rs
+++ examples/stdinout.rs
@@ -10,8 +10,8 @@ fn main() {
     let window_size = 1024;
     let slide_size = window_size / 4;
     let envelope_order = window_size / 8;
-    let formant = 0.2;
-    let pitch = 0.4;
+    let formant = (-0.2f32).exp2();
+    let pitch = (-0.4f32).exp2();
 
     let process = api::voice_change(
         windows::hann_window(window_size),
